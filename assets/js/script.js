23ae04c4aa2a57c7f8aa8ea1d36a594e5082435d
Santiago <sagos0919@gmail.com>
2022-12-11 17:10:03 +0000

    starter code and some pseudocode added

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+'use strict';
+// #PSEUDOCODE
+// * TASK 1
+// todo
+// Create a table with bootstrap
+// The table must have three columns...
+// HOUR
+// EVENT (Text area)
+// SAVE BUTTON
+
+// todo
+// Each table row should be dynamically get a background color based
+
+// * TASK 2
+// todo
+// At top show the current day
+// Each time block should have a color code...
+// PAST: grey
+// CURRENT: orange
+// AHEAD: lightgreen
+
+// * TASK 3
+// todo
+// Use local storage to keep the entered data after SAVE BUTTON is clicked
+// The events persist even after refresh
+
+// TODO: As bonus create a button at the bottom that clears out the app
